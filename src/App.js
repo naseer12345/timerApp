cf57cd9359cd18c307a114cfb41ddacadbc21ac3
naseer12345 <naseer.khan00128@gmail.com>
2023-08-26 12:24:33 +0100

    working version of my app

--- src/App.js
+++ src/App.js
@@ -1,49 +1,27 @@
 import { useEffect, useState } from 'react';
 import './App.css';
 
-function SettingUpTimer({showTimerDiv, setShowTimerDiv,timerMinutes,setTimerMinutes,timerSeconds,setTimerSeconds}) {
-  
-
-
-
-
+function SettingUpTimer({ showTimerDiv, setShowTimerDiv, timerMinutes, setTimerMinutes, timerSeconds, setTimerSeconds }) {
 
   return (
     <div className='timerdiv'>
-      <input type="number" placeholder="Minutes." onChange={(e) => setTimerMinutes(e.target.value)}></input>
-      <input type="number" placeholder="Seconds"  onChange={(e) => setTimerSeconds(e.target.value)}></input>
-      <button onClick={()=> setShowTimerDiv(!showTimerDiv)}>finish</button>
+      <input type="number" placeholder="Minutes." onChange={(e) => setTimerMinutes(Number(e.target.value))}></input>
+      <input type="number" placeholder="Seconds" onChange={(e) => setTimerSeconds(Number(e.target.value))}></input>
+      <button onClick={() => setShowTimerDiv(!showTimerDiv)}>finish</button>
     </div>
   );
 }
-
-
-
-
 
 function App() {
   const [condition, setCondition] = useState(true);
   const [seconds, setSeconds] = useState(0);
   const [minutes, setMinutes] = useState(0);
   const [hours, setHours] = useState(0);
+  
   const [timerMinutes, setTimerMinutes] = useState(0);
   const [timerSeconds, setTimerSeconds] = useState(0);
   const [showTimerDiv, setShowTimerDiv] = useState(false);
-  const [timerStatus,setTimerStatus]= useState("No timer has been setup")
-
-
-
-
-  useEffect(()=>{
-    console.log("the button is working")
-    if(timerSeconds > 0){
-      console.log("its working")
-    }
-  },[showTimerDiv])
-
-
-
-
+  const [timerStatus, setTimerStatus] = useState("No timer has been setup");
 
   useEffect(() => {
     if (!condition) {
@@ -55,9 +33,19 @@ function App() {
           return prevSeconds + 1; 
         });
       }, 1000);
+      if (seconds>0){
+      if (minutes === timerMinutes && seconds === timerSeconds) {
+        setTimerStatus("Timer finished!");
+        
+          window.open('https://www.youtube.com/watch?v=BBJa32lCaaY', '_blank');
+        
+        
+      }
+    }
+      
       return () => clearInterval(interval);
     }
-  }, [condition]);
+  }, [condition, seconds]);
 
   useEffect(() => {
     if (seconds === 0 && minutes < 59) {
@@ -71,6 +59,8 @@ function App() {
   return (
     <div className='main'>
       <div className="App">
+      <p>This website is not currently optamized for mobile or small screen devices i know it looks kinda shit, sorry.</p>
+
         <h1 className='timer'>{hours}.{minutes}.{seconds}</h1>
         <div className='buttonsdiv'>
           <button onClick={() => setCondition(!condition)}>{condition ? 'start' : 'stop'}</button>
@@ -81,7 +71,7 @@ function App() {
           }}>Reset</button>
           <button onClick={() => setShowTimerDiv(!showTimerDiv)}>Set Timer</button>
         </div>
-        { showTimerDiv ? <SettingUpTimer showTimerDiv={showTimerDiv} setShowTimerDiv={setShowTimerDiv} /> : <h1>{timerStatus}</h1>}
+        {showTimerDiv ? <SettingUpTimer showTimerDiv={showTimerDiv} setShowTimerDiv={setShowTimerDiv} timerMinutes={timerMinutes} setTimerMinutes={setTimerMinutes} timerSeconds={timerSeconds} setTimerSeconds={setTimerSeconds} /> : <h1>{timerStatus}</h1>}
       </div>
     </div>
   );
